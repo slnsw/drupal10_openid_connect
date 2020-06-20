76d2a39cd685716a68b5a0c4e0d2187f625b7664
robin.ingelbrecht <robin.ingelbrecht@2339074.no-reply.drupal.org>
2020-06-21 01:21:38 +0200

    Issue #3073495 by robin.ingelbrecht, jcnventura: support boolean fields in user accounts

--- src/OpenIDConnect.php
+++ src/OpenIDConnect.php
@@ -692,6 +692,10 @@ class OpenIDConnect {
                 $account->set($property_name, $claim_value);
                 break;
 
+              case 'boolean':
+                $account->set($property_name, !empty($claim_value));
+                break;
+
               case 'image':
                 // Create file object from remote URL.
                 $basename = explode('?', $this->fileSystem->basename($claim_value))[0];
