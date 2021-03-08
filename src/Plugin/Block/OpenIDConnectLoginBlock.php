c6191a47df237a2a6b765e087fd5c2a4adeb1418
jedihe <jedihe@350711.no-reply.drupal.org>
2021-03-09 00:33:04 +0100

    Issue #3193374 by jedihe, jcnventura: Incorrect typehint prevents instancing login block

--- src/Plugin/Block/OpenIDConnectLoginBlock.php
+++ src/Plugin/Block/OpenIDConnectLoginBlock.php
@@ -33,12 +33,12 @@ class OpenIDConnectLoginBlock extends BlockBase implements ContainerFactoryPlugi
    *   A configuration array containing information about the plugin instance.
    * @param string $plugin_id
    *   The plugin_id for the plugin instance.
-   * @param string $plugin_definition
+   * @param mixed $plugin_definition
    *   The plugin implementation definition.
    * @param \Drupal\Core\Form\FormBuilder $form_builder
    *   The form builder.
    */
-  public function __construct(array $configuration, string $plugin_id, string $plugin_definition, FormBuilder $form_builder) {
+  public function __construct(array $configuration, string $plugin_id, $plugin_definition, FormBuilder $form_builder) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->formBuilder = $form_builder;
   }
