e69296a4b59b403714ea76db853964e7fea92428
slasher13 <slasher13@3054025.no-reply.drupal.org>
2021-06-15 19:00:00 +0200

    Issue #3215229 by Daniel Kulbe, slasher13, jcnventura: Okta integration, Logout fails to send client_id, results in error

--- src/Controller/OpenIDConnectRedirectController.php
+++ src/Controller/OpenIDConnectRedirectController.php
@@ -311,7 +311,7 @@ class OpenIDConnectRedirectController implements ContainerInjectionInterface, Ac
               'query' => ['id_token_hint' => $this->session->retrieveIdToken()],
             ];
             if ($redirect_logout_url) {
-              $url_options['query']['post_logout_redirect_uri'] = $redirect_logout_url->setAbsolute()->toString();
+              $url_options['query']['post_logout_redirect_uri'] = $redirect_logout_url->setAbsolute()->toString(TRUE)->getGeneratedUrl();
             }
             $redirect = Url::fromUri($endpoints['end_session'], $url_options)->toString(TRUE);
             $response = new TrustedRedirectResponse($redirect->getGeneratedUrl());
